69860a324a837e71e822f7bc5081aab318553fa2
annatrajano <annatrajano95@gmail.com@example.com>
2023-03-03 14:33:49 -0300

    feat: :fire: add FormInput component

--- organo/src/components/formInput/formInput.js
+++ organo/src/components/formInput/formInput.js
@@ -1,10 +1,12 @@
-const FormInput = () => {
+import "./formInput.css";
+
+const FormInput = (props) => {
 
         return (
 
-            <div>
-                <label>Nome</label>
-                <input />
+            <div className="form-input">
+                <label>{props.label}</label>
+                <input placeholder={props.placeholder}/>
             </div>
         )
 
