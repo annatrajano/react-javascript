f32a688e13bac55fd9e7f4f88f00351d5f241b37
annatrajano <annatrajano95@gmail.com@example.com>
2023-04-13 20:05:26 -0300

    feat: :fire: add About page with elements

--- helloworld/src/routes.js
+++ helloworld/src/routes.js
@@ -1,3 +1,4 @@
+import PaginaPadrao from "componentes/PaginaPadrao";
 import Rodape from "componentes/Rodape";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "./componentes/Menu";
@@ -9,8 +10,10 @@ function AppRoutes() {
     <BrowserRouter>
       <Menu />
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route index element={<Inicio />} />
+          <Route path="sobremim" element={<SobreMim />} />
+        </Route>
         <Route path="*" element={<div>Página não encontrada</div>} />
       </Routes>
       <Rodape />
