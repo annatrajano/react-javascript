c962af8a9295c0784671e85da99540d97f7470cf
annatrajano <annatrajano95@gmail.com@example.com>
2023-04-18 21:00:19 -0300

    [add] Post cards and NotFound page

--- helloworld/src/routes.js
+++ helloworld/src/routes.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "./componentes/Menu";
 import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
+import Post from "paginas/Post";
+import NaoEncontrada from "paginas/NaoEncontrada";
 
 function AppRoutes() {
   return (
@@ -13,8 +15,11 @@ function AppRoutes() {
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Inicio />} />
           <Route path="sobremim" element={<SobreMim />} />
+          <Route path="posts/:id" element={<Post />} />
+          
+
         </Route>
-        <Route path="*" element={<div>Página não encontrada</div>} />
+        <Route path="*" element={<NaoEncontrada />} />
       </Routes>
       <Rodape />
     </BrowserRouter>
