fd99376bb1b5c830b26a4047fed8db13df532f39
annatrajano <annatrajano95@gmail.com@example.com>
2023-05-23 20:43:49 -0300

    feat: :art: improve about me

--- helloworld/src/paginas/SobreMim/index.js
+++ helloworld/src/paginas/SobreMim/index.js
@@ -23,23 +23,14 @@ export default function SobreMim() {
             />
 
             <p className={styles.paragrafo}>
-                Oi, tudo bem? Eu sou instrutor de Front-end na Alura e estou feliz de te ver por aqui.
+                Oi, tudo bem? Eu trabalho como Desenvolvedora Front-end na PUC-RIO e estou feliz de te ver por aqui.
             </p>
             <p className={styles.paragrafo}>
-                Minha história com programação começou no Instituto Federal do Piauí (IFPI), quando fiz o ensino médio integrado ao curso de Informática. Eu aprendi lógica de programação e o básico de várias linguagens, como PHP, Python, Java, mas sem aprofundar muito em cada uma delas. Eu gostava muito de estudar programação, mas na época não fazia ideia de que trabalharia com isso hoje.
+                Minha história com programação começou no período da faculdade de Engenharia, onde tive o primeiro contato com a lógica 
+                da programação e algumas linguagens. Posteriormente, com o término da minha graduação em Engenharia de Petróleo, iniciei o curso da Trybe, uma escola voltada para o Desenvolvimento Web. Finalizei o curso e me formei como Desenvolvedora FullStack.
             </p>
             <p className={styles.paragrafo}>
-                No ensino superior, escolhi cursar Engenharia Elétrica na Universidade Federal do Piauí (UFPI). Lá eu consegui entrar no PET (Programa de Educação Tutorial), um grupo acadêmico que contribui de diversas formas para o curso. Lá eu tive a oportunidade de estudar desenvolvimento web para ser um dos mantenedores do site deles.
-            </p>
-            <p className={styles.paragrafo}>
-                Com isso tive minha experiência de dev mais próxima da realidade, com prazos de implementação para o site, e aprendi muito enquanto codificava.
-            </p>
-            <p className={styles.paragrafo}>
-                E foi enquanto eu estava no PET que decidi procurar um estágio em desenvolvimento web. Eu encontrei uma vaga na Alura para o Scuba Team e percebi que ela aliava duas paixões minhas: programação e educação. Para minha felicidade, fui escolhido para fazer parte do time.
-
-            </p>
-            <p className={styles.paragrafo}>
-                Desde então, tem sido aprenas aprendizados atrás de aprendizados. A Alura é uma escola não só para seus alunos e alunas, mas também para os colaboradores e colaboradoras. Hoje sou muito feliz de ter a oportunidade de trazer esses conteúdos ricos e encantadores para você. Espero que aprenda bastante!
+                Desde então, após começar a trabalhar com desenvolvimento de aplicações, optei por focar no Front-end, minha verdadeira paixão! Hoje sou muito feliz de ter migrado para área da tecnologia e tenho como meta aprender cada vez mais :)
             </p>
       </PostModelo>
 
