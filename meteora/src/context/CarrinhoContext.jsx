75298cf223b81f33fc061cc0bae9405cb5cb0920
annatrajano <annatrajano95@gmail.com@example.com>
2024-01-04 16:34:36 -0300

    feat: :zap: creating context api

---
+++ meteora/src/context/CarrinhoContext.jsx
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+export const CarrinhoContext = createContext();
+
+export const CarrinhoProvider = ({children}) => {
+
+    return (
+        <CarrinhoContext.Provider>
+            {children}
+        </CarrinhoContext.Provider>
+    )
+
+}
