25c861c984850c55f6e814d439f7357c34a23bed
annatrajano <annatrajano95@gmail.com@example.com>
2024-01-21 16:40:07 -0300

    feat: :tada: begin a project

--- meteora/src/context/CarrinhoContext.jsx
+++ meteora/src/context/CarrinhoContext.jsx
@@ -1,13 +1,48 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const CarrinhoContext = createContext();
 
-export const CarrinhoProvider = ({children}) => {
+export const CarrinhoProvider = ({ children }) => {
+  const [carrinho, setCarrinho] = useState([]);
+  const [quantidadeProdutos, setQuantidadeProdutos] = useState(0);
+  const [valorTotalCarrinho, setValorTotalCarrinho] = useState(0);
 
-    return (
-        <CarrinhoContext.Provider>
-            {children}
-        </CarrinhoContext.Provider>
-    )
+  function removerProduto(id) {
+    const produto = carrinho.find((itemDoCarrinho) => itemDoCarrinho.id === id);
+    const ehOUltimo = produto.quantidade === 1;
+    if (ehOUltimo) {
+      return setCarrinho((carrinhoAnterior) =>
+        carrinhoAnterior.filter((itemDoCarrinho) => itemDoCarrinho.id !== id)
+      );
+    }
+    setCarrinho(mudarQuantidade(id, -1));
+  }
 
-}
+  function removerProdutoCarrinho(id) {
+    const produto = carrinho.filter(
+      (itemDoCarrinho) => itemDoCarrinho.id !== id
+    );
+    setCarrinho(produto);
+  }
+
+  useEffect(() => {
+    const { novoTotal, novaQuantidade } = carrinho.reduce(
+      (contador, produto) => ({
+        novaQuantidade: contador.novaQuantidade + produto.quantidade,
+        novoTotal: contador.novoTotal + produto.preco * produto.quantidade,
+      }),
+      {
+        novaQuantidade: 0,
+        novoTotal: 0,
+      }
+    );
+    setQuantidadeProdutos(novaQuantidade);
+    setValorTotalCarrinho(novoTotal);
+  }, [carrinho, setQuantidadeProdutos, setValorTotalCarrinho]);
+
+  return (
+    <CarrinhoContext.Provider value={{ carrinho, setCarrinho, quantidadeProdutos, setQuantidadeProdutos, valorTotalCarrinho, setValorTotalCarrinho  }}>
+      {children}
+    </CarrinhoContext.Provider>
+  );
+};
